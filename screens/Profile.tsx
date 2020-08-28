04a07a42e95d16647ff73fb9b5aa63e8f80aaba0
Tony <tony.yang98@gmail.com>
2020-08-28 19:22:45 -0400

    edit styling

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, FlatList, TouchableOpacity, ImageBackground } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Card } from 'react-native-elements';
@@ -58,58 +58,59 @@ export default () => {
   return (
 
     <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
+      <ImageBackground source={require('../assets/images/background.png')} style={styles.image}>
+        <Text style={styles.title}>Profile</Text>
 
-      <View></View>
+        <View></View>
 
-      <View style={{ marginTop: '8%', marginLeft: '2%', flex: 1, flexDirection: 'row' }}>
-        <Image
-          source={require('../assets/images/sample.png')}
-          style={{ width: 150, height: 150, borderRadius: 400 / 2, backgroundColor: 'white', borderColor: 'white' }}
-        />
+        <View style={{ marginTop: '8%', marginLeft: '2%', flex: 1, flexDirection: 'row' }}>
+          <Image
+            source={require('../assets/images/sample.png')}
+            style={{ width: 150, height: 150, borderRadius: 400 / 2, backgroundColor: 'white', borderColor: 'white' }}
+          />
 
-        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <View style={{ flex: 1, flexDirection: 'column' }}>
 
-          <Text style={{ fontSize: 30, fontWeight: 'bold', marginLeft: '10%' }}>
-            Enoch Chen
+            <Text style={{ fontSize: 30, fontWeight: 'bold', marginLeft: '10%' }}>
+              Enoch Chen
           </Text>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: '10%', marginTop: '5%' }}>
-            enochen.me
+            <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: '10%', marginTop: '5%' }}>
+              enochen.me
           </Text>
+          </View>
         </View>
-      </View>
 
-      <View style={{ marginTop: '7%', top: '12.5%' }}>
-        <Text style={{
-          fontSize: 30,
-          fontWeight: 'bold',
-          marginLeft: '5%',
-        }}>
-          Past Moods
+        <View style={{ marginTop: '7%', top: '12.5%' }}>
+          <Text style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            marginLeft: '5%',
+          }}>
+            Past Moods
         </Text>
 
 
-        <Card containerStyle={{ backgroundColor: '#FAE3FF', borderColor: '#FAE3FF', borderRadius: 10, alignContent: 'center', marginTop: '5%', height: '65%' }}>
-          <ScrollView>
-            <FlatList
-              data={DATA}
-              renderItem={renderItem}
-              keyExtractor={(item) => item.id}
-              extraData={selectedId}
-            />
+          <Card containerStyle={{ backgroundColor: '#FAE3FF', borderColor: '#FAE3FF', borderRadius: 10, alignContent: 'center', marginTop: '5%', height: '65%' }}>
+            <ScrollView>
+              <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                extraData={selectedId}
+              />
 
-          </ScrollView>
-        </Card>
-
-
-      </View>
-      <Button
-        title="Sign Out"
-        style={{ marginBottom: '5%', width: '90%', justifyContent: 'center', alignSelf: 'center', borderRadius: 10 }}
-        onPress={signOut}
-      />
+            </ScrollView>
+          </Card>
 
 
+        </View>
+        <Button
+          title="Sign Out"
+          style={{ marginBottom: '5%', width: '90%', justifyContent: 'center', alignSelf: 'center', borderRadius: 10 }}
+          onPress={signOut}
+        />
+
+      </ImageBackground>
     </View>
 
   );
@@ -119,6 +120,11 @@ const styles = StyleSheet.create({
   container: {
     marginTop: '3%',
     flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
   },
   title: {
     fontSize: 30,
