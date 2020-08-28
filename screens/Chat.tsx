df9e599423a01e2833378ebedbd0548131a7a2e6
Tony <tony.yang98@gmail.com>
2020-08-28 11:10:01 -0400

    start chat

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -1,15 +1,84 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+import { dialogflowConfig } from '../env';
+import { useState, useEffect } from 'react';
+
+type User = {
+  _id: number,
+  name: string,
+  avatar: string
+}
+
+type Message = {
+  _id: number,
+  text: string,
+  createdAt: Date,
+  user: User
+}
+
+const BOT_USER: User = {
+  _id: 2,
+  name: 'Health Bot',
+  avatar: 'https://previews.123rf.com/images/iulika1/iulika11909/iulika1190900021/129697389-medical-worker-health-professional-avatar-medical-staff-doctor-icon-isolated-on-white-background-vec.jpg'
+};
 
 export default function Chat() {
+
+  const [messages, setMessages] = useState<Message[]>([{
+    _id: 1,
+    text: 'Hi! I am the Moody 🤖.\n\nHow may I help you today?',
+    createdAt: new Date(),
+    user: BOT_USER
+  }]);
+
+  useEffect(() => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  }, []);
+
+  const handleGoogleResponse = (result: any) => {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    sendBotResponse(text);
+  }
+
+  const onSend = (messages: Message[] = []) => {
+    setMessages(prev => GiftedChat.append(prev, messages))
+    Dialogflow_V2.requestQuery(
+      messages[0].text,
+      result => handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+
+  const sendBotResponse = (text: string) => {
+    const msg = {
+      _id: messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+
+    setMessages(prev => GiftedChat.append(prev, [msg]))
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/Chat.tsx" />
+      <GiftedChat
+        messages={messages}
+        onSend={onSend}
+        user={{
+          _id: 1
+        }}
+      />
     </View>
   );
 }
@@ -17,8 +86,6 @@ export default function Chat() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
